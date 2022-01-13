e7ade2dc798af5ff9d5af3678e906d36f1e249a9
sayedulabrar <96303632+sayedulabrar@users.noreply.github.com>
2022-01-13 21:08:57 +0600

    message sent

--- new.js
+++ new.js
@@ -170,57 +170,37 @@ function moment() {
 
 
 
+    function showed() {
+
+      var name = document.getElementById("Name").value;
+      var sub= document.getElementById("Sub").value;
+      var email= document.getElementById("Email").value;
+      var mes= document.getElementById("Mes").value;
+
+      
+      
+  
+       firebase.database().ref('User/' + name).set({
+              name : name,
+              sub : sub,
+              mes : mes,
+              email : email
+            }, function(error) {
+              if (error) {
+                // The write failed...
+              } else {
+                document.getElementById("Name").value = "";
+                document.getElementById("Sub").value = "";
+                document.getElementById("Email").value = "";
+                document.getElementById("Mes").value="";
+
+                  alert("DONE");
+                
+             
+              }
+            });
+     
+   } 
 
 
 
-
-//     let result = [];
-
-// function sho(e) {
-//   var name = document.getElementById("name").value;
-//   var message = document.getElementById("message").value;
-//   var email = document.getElementById("email").value;
-
-//   firebase
-//     .database()
-//     .ref("User/" + message)
-//     .set(
-//       {
-//         name: name,
-//         message: message,
-//         email: email,
-//       },
-//       function (error) {
-//         if (error) {
-//           // The write failed...
-//           console.log(error);
-//         } else {
-//           // alert("DONE");
-//           result.push({
-//             name,
-//             message,
-//             email,
-//           });
-
-//           document.getElementById("name").value = "";
-//           document.getElementById("message").value = "";
-//           document.getElementById("email").value = "";
-
-//           document.getElementById("id2").innerHTML = result
-//             .map(
-//               (el) =>
-//                 `<div class="comment-text">
-//                 <h4>
-//                     <a href="">${el.name} </a> 
-//                 </h4>
-//                 <p>${el.email}</p> 
-//                 <h3>
-//                     ${el.message}
-//                 </h3>
-//             </div>`
-//             )
-//             .join("");
-//         }
-//       }
-//     );
-// }
